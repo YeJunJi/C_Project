899d331c2fd3a11e323104ab7b1656cd9db69037
YeJun <yj201244@gmail.com>
2021-02-14 22:26:23 +0900

    2021/02/14

--- baek/al/st/1935.c
+++ baek/al/st/1935.c
@@ -1,99 +1,39 @@
 #include <stdio.h>
 
 int main(){
-    int n, p=0, q=0;
+    int n, p=0;
+    double inputNum[101]={0}, res[101]={0};
     scanf("%d", &n);
-    char input[101] = {0}, st[101];
+    char input[101]={0};
     scanf("%s", input);
-    double inputNum[n];
-    double stack=0, res=0, stn[2]={0};
     for(int i=0;i<n;i++){
         scanf("%lf", &inputNum[i]);
     }
 
     for(int i=0;i<101;i++){
         if(input[i]==0){
-            if(stack!=0){
-                res+=stack;
-            }
             break;
         }
-        if('A'<=input[i]&&input[i]<='Z'){
-            stack=0;
-            st[p++] = input[i];
+        if('A'<=input[i] && input[i]<='Z'){
+            res[p++] = inputNum[input[i]-65];
         }
         else if(input[i]=='*'){
-            if(stack==0 && p>1){
-                stack=inputNum[(int)st[p-2]-65]*inputNum[(int)st[p-1]-65];
-                p-=2;
-            }
-            else if(stack==0 && p==1){
-                res*=inputNum[(int)st[p-1]-65];
-                p--;
-            }
-            else if(stack!=0 && p==0){
-                res*=stack;
-                stack=0;
-            }
-            else{
-                stack*=inputNum[(int)st[p-1]-65];
-                p--;
-            }
+            res[p-2] *= res[p-1];
+            p--;
         }
         else if(input[i]=='/'){
-            if(stack==0 && p>1){
-                stack=inputNum[(int)st[p-2]-65]/inputNum[(int)st[p-1]-65];
-                p-=2;
-            }
-            else if(stack==0 && p==1){
-                res/=inputNum[(int)st[p-1]-65];
-                p--;
-            }
-            else if(stack!=0 && p==0){
-                res/=stack;
-                stack=0;
-            }
-            else{
-                stack/=inputNum[(int)st[p-1]-65];
-                p--;
-            }
+            res[p-2] /= res[p-1];
+            p--;
         }
         else if(input[i]=='+'){
-            if(stack==0 && p>1){
-                stack=inputNum[(int)st[p-2]-65]+inputNum[(int)st[p-1]-65];
-                p-=2;
-            }
-            else if(stack==0 && p==1){
-                res+=inputNum[(int)st[p-1]-65];
-                p--;
-            }
-            else if(stack!=0 && p==0){
-                res+=stack;
-                stack=0;
-            }
-            else{
-                stack+=inputNum[(int)st[p-1]-65];
-                p--;
-            }
+            res[p-2] += res[p-1];
+        
+            p--;
         }
         else if(input[i]=='-'){
-            if(stack==0 && p>1){
-                stack=inputNum[(int)st[p-2]-65]-inputNum[(int)st[p-1]-65];
-                p-=2;
-            }
-            else if(stack==0 && p==1){
-                res-=inputNum[(int)st[p-1]-65];
-                p--;
-            }
-            else if(stack!=0 && p==0){
-                res-=stack;
-                stack=0;
-            }
-            else{
-                stack-=inputNum[(int)st[p-1]-65];
-                p--;
-            }
+            res[p-2] -= res[p-1];
+            p--;
         }
-        printf("%0.2lf\n", res);
     }
+    printf("%0.2lf", res[0]);
 }
