ff6d0b76f42a9293e3db790fcf6b49099ffee4d7
YeJun <yj201244@gmail.com>
2021-02-24 22:56:38 +0900

    2021/02/24

--- baek/al/dp/9251.c
+++ baek/al/dp/9251.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
+int max(int a, int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
 int main(){
-    char input1[1001]={0}, input2[1001]={0};
+    char input1[1001]={0}, input2[1001]={0}, m=0;
     scanf("%s", input1);
     scanf("%s", input2);
+    int dp[1001][1001]={0};
     int len1 = strlen(input1);
     int len2 = strlen(input2);
-    printf("%d\n", len1);
-    printf("%d\n", len2);
+    for (int i = 1; i < len2+1; i++)
+    {
+        for (int j = 1; j < len1+1; j++)
+        {
+            if(input2[i-1]==input1[j-1]){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+            if(m<dp[i][j]){
+                m=dp[i][j];
+            }
+        }
+        
+    }
+    for(int i=0;i<len2+1;i++){
+        for(int j=0;j<len1+1;j++){
+            printf("%d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+    printf("%d\n", m);
 }
