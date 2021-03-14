ac7facbd2083bb8c2b4adc135be222759d273f38
YeJun <yj201244@gmail.com>
2021-03-15 01:29:10 +0900

    2021/03/14

--- baek/al/dp/2565.c
+++ baek/al/dp/2565.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
-int main(){
-    int n, input[101]={0}, dp[101]={0}, max=0;
-    scanf("%d", &n);
-    for(int i=0;i<n;i++){
-        int tmp;
-        scanf("%d", &tmp);
-        scanf("%d", &input[tmp]);
+int N;
+int M = 0;
+int L[100][3]; // [0] = S, [1] = D, [2] = LIS
 
+int main() {
+    
+    scanf("%d", &N);
+    
+    for(int i = 0; i < N; i++) {
+        scanf("%d %d", &L[i][0], &L[i][1]);
     }
-    for(int i=1;i<=101;i++){
-        if(input[i]!=0){
-            dp[i]=1;
-        }
-        for(int j=input[i];j<=101;j++){
-            if(input[j]>input[i])
+    
+    // L 기준으로 오름차순 정렬.
+    for(int i=0;i<N;i++){
+        for(int j=i;j<N;j++){
+            if(L[i][0]>L[j][0]){
+                int x, y;
+                x = L[i][0]; y = L[i][1];
+                L[i][0] = L[j][0]; L[i][1] = L[j][1];
+                L[j][0] = x; L[j][1] = y;                
+            }
         }
     }
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < i; j++) {
+            if(L[i][1] > L[j][1] && L[i][2] <= L[j][2])
+                L[i][2] = L[j][2] + 1;
+        }
+        if(M < L[i][2])
+            M = L[i][2];
+    }
+    
+    printf("%d\n", N - (M + 1));
+    
 }
